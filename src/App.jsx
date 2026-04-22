0e7506331b0044cb293a2247749241e4cf491454
MimmoClaude <mimmoclaude@gmail.com>
2026-04-22 20:11:18 +0200

    Fix Android OAuth redirect URI

--- src/App.jsx
+++ src/App.jsx
@@ -162,7 +162,7 @@ export function App() {
           scope: "email profile https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/gmail.send",
           resourceUrl: "https://www.googleapis.com/oauth2/v3/userinfo",
           web: { appId: cid, responseType: "token" },
-          android: { appId: cid, responseType: "code", redirectUrl: "com.scadenze.app:/" },
+          android: { appId: cid, responseType: "code", redirectUrl: "com.scadenze.app:/oauth2redirect" },
           pkceEnabled: true,
         });
         const tk = resp["access_token"];
